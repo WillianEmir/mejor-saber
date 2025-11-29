8ef496d387e02e00ad6627cddac65625c344577c
wemhack <99555303+wemhack@users.noreply.github.com>
2025-11-29 17:20:39 -0500

    last commit

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,14 +1,6 @@
 import 'dotenv/config'
-import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '../generated/prisma'
 
-const connectionString = process.env.DATABASE_URL;
-
-if (!connectionString) {
-  throw new Error('DATABASE_URL is not set');
-}
-
-const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient();
 
 export default prisma;
